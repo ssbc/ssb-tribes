fd9dcbeb2b2d0d4b6ebab1e3112894ba17820e6d
mixmix <whimful@gmail.com>
2020-02-11 12:59:49 +1300

    add membership adding, key getting

--- keychain.js
+++ keychain.js
@@ -2,33 +2,70 @@ const { mkdirSync } = require('fs')
 const level = require('level')
 const charwise = require('charwise')
 const pull = require('pull-stream')
-const pullLevel  = require('pull-level')
+const { read }  = require('pull-level')
+
+const GROUP = 'group'
+const MEMBER = 'member'
 
 module.exports = function Keychain (path) {
   mkdirSync(path, { recursive: true })
 
   const db = level(path, { valueEncoding: charwise })
 
+  const group = {
+    add (groupId, groupKey, cb) {
+      // TODO check groupId and groupKey are valid...
+      db.put([GROUP, groupId, Date.now()], groupKey, cb)
+    },
+    addAuthor (groupId, authorId, cb) {
+      db.put([MEMBER, authorId, groupId], groupId, cb)
+    },
+    list (cb) {
+      pull(
+        read(db, {
+          lt: [GROUP + '~', undefined, undefined], // "group~" is just above "group" in charwise sort
+          gt: [GROUP, null, null]
+        }),
+        pull.map(({ key, value }) => {
+          var groupId = key.split(',')[1]
+          return { [groupId]: value }
+        }),
+        pull.collect((err, pairs) => {
+          if (err) return cb(err)
+          cb(null, Object.assign(...pairs))
+        })
+      )
+    },
+    key (groupId, cb) {
+      pull(
+        read(db, {
+          lt: [GROUP, groupId + '~', undefined], // "group~" is just above "group" in charwise sort
+          gt: [GROUP, groupId, null]
+        }),
+        pull.map(({ value }) => value),
+        pull.take(1),
+        pull.collect((err, keys) => {
+          if (err) return cb(err)
+          cb(null, keys[0])
+        })
+      )
+    }
+  }
+
   return {
-    group: {
-      add(groupId, groupKey, cb) {
-        // TODO check groupId and groupKey are valid...
-        db.put(['key', groupId, Date.now()], groupKey, cb)
-      },
-      list(cb) {
-        const result = {}
+    group,
+    author: {
+      keys (authorId, cb) {
         pull(
-          pullLevel.read(db, {
-            lte: ['key~', undefined, undefined], // "key~" is just above "key" in charwise sort
-            gte: ['key', null, null]
+          read(db, {
+            // lt: [MEMBER + '~', authorId + '~', undefined],
+            gt: [MEMBER, authorId, null]
           }),
-          pull.through(({ key, value }) => {
-            const groupId = key.split(',')[1]
-            result[groupId] = value
-          }),
-          pull.collect((err) => {
+          pull.map(({ value }) => value),
+          pull.asyncMap(group.key),
+          pull.collect((err, keys) => {
             if (err) return cb(err)
-            cb(null, result)
+            cb(null, keys)
           })
         )
       }
--- test/keychain.test.js
+++ test/keychain.test.js
@@ -2,17 +2,40 @@ const test = require('tape')
 const Keychain = require('../keychain')
 
 test('keychain', t => {
-  const path = `/tmp/keychain-${Date.now()}`
+  const tests = [
+    () => {
+      const path = `/tmp/keychain-${Date.now()}`
+      const keychain = Keychain(path)
 
-  const keychain = Keychain(path)
+      keychain.group.add('groupId_A', 'groupKey_A', (err, data) =>{
+        keychain.group.list((err, data) => {
+          t.deepEqual(data, { groupId_A: 'groupKey_A' }, 'group.add')
 
-  keychain.group.add('groupId_A', 'groupKey_A', (err, data) =>{
-    console.log(err, data)
-    keychain.group.list((err, data) => {
-      t.deepEqual(data, { groupId_A: 'groupKey_A' }, 'group.add')
+          keychain.close()
+        })
+      })
+    },
 
-      keychain.close()
-      t.end()
-    })
-  })
+    () => {
+      const path = `/tmp/keychain-${Date.now()}`
+      const keychain = Keychain(path)
+
+      keychain.group.add('groupId_A', 'groupKey_A', (_, __) =>{
+        keychain.group.add('groupId_B', 'groupKey_B', (_, __) =>{
+          keychain.group.addAuthor('groupId_A', '@mix', (_, __) => {
+            keychain.group.addAuthor('groupId_B', '@mix', (_, __) => {
+              keychain.author.keys('@mix', (err, keys) => {
+                t.deepEqual(keys, ['groupKey_A', 'groupKey_B'], 'member.keys')
+
+                keychain.close()
+              })
+            })
+          })
+        })
+      })
+    }
+  ]
+
+  t.plan(tests.length)
+  tests.forEach(i => i())
 })
