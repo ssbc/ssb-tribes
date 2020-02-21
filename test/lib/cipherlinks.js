202fa923f402e4068d47d42f1a79195c871de61b
mixmix <whimful@gmail.com>
2020-02-21 13:40:49 +1300

    first pass on cipherlink helper

---
+++ test/lib/cipherlinks.js
@@ -0,0 +1,32 @@
+const test = require('tape')
+const { FeedId, MsgId } = require('../../lib/cipherlinks')
+
+test('Cipherlink/FeedId', t => {
+  var feedId = '@YXkE3TikkY4GFMX3lzXUllRkNTbj5E+604AkaO1xbz8=.ed25519'
+  var id = new FeedId(feedId)
+
+  var expected = Buffer.concat([
+    Buffer.from([0]),  // type = feed
+    Buffer.from([0]),  // format = classic
+    Buffer.from('YXkE3TikkY4GFMX3lzXUllRkNTbj5E+604AkaO1xbz8=', 'base64')
+  ])
+
+  t.deepEqual(id.toTFK(), expected, 'toTFK')
+  t.deepEqual(id.toSSB(), feedId, 'toSSB')
+  t.end()
+})
+
+test('Cipherlink/MsgId', t => {
+  var msgId = '%onDYxSjsIb4d3KhgHC7g5wdHLWw/7zygIBvZEx7v6KU=.sha256'
+  var id = new MsgId(msgId)
+
+  var expected = Buffer.concat([
+    Buffer.from([1]),  // type = feed
+    Buffer.from([0]),  // format = classic
+    Buffer.from('onDYxSjsIb4d3KhgHC7g5wdHLWw/7zygIBvZEx7v6KU=', 'base64')
+  ])
+
+  t.deepEqual(id.toTFK(), expected, 'toTFK')
+  t.deepEqual(id.toSSB(), msgId, 'toSSB')
+  t.end()
+})
