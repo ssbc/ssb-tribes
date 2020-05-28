751bf03869a051580a6ee57a9659f3425501f31c
mixmix <whimful@gmail.com>
2020-05-28 14:17:13 +1200

    add direct-message-key funciton

--- test/generate/direct-messages.js
+++ test/generate/direct-messages.js
@@ -1,1 +1,29 @@
-module.exports = {}
+const keys = require('ssb-keys')
+const { print } = require('../helpers')
+const directMessageKey = require('../../lib/direct-message-key')
+
+const generators = [
+  (i) => {
+    const myKeys = keys.generate()
+    const yourKeys = keys.generate()
+
+    const sk = Buffer.from(myKeys.private, 'base64')
+    const pk = Buffer.from(yourKeys.public, 'base64')
+    const sharedKey = directMessageKey(sk)(pk)
+
+    const vector = {
+      type: 'direct_message_shared_key',
+      description: 'calculate a shared DM key for another feedID',
+      input: {
+        my_keys: myKeys,
+        feed_id: yourKeys.id
+      },
+      output: {
+        shared_key: sharedKey
+      }
+    }
+    print(`vectors/direct-message-key${i + 1}.json`, vector)
+  }
+]
+
+generators.forEach((fn, i) => fn(i))
---
+++ test/lib/direct-message-key.test.js
@@ -0,0 +1,52 @@
+const test = require('tape')
+const { generate } = require('ssb-keys')
+const vectors = [
+  require('private-group-spec/vectors/direct-message-key1.json')
+]
+
+const { decodeLeaves } = require('../helpers')
+const directMessageKey = require('../../lib/direct-message-key')
+
+test('direct-message-key', t => {
+  const myKeys = makeKeys()
+  const yourKeys = makeKeys()
+
+  t.deepEqual(
+    directMessageKey(myKeys.secret)(yourKeys.public),
+    directMessageKey(yourKeys.secret)(myKeys.public),
+    'the key is shared!'
+  )
+
+  vectors.forEach(vector => {
+    decodeLeaves(vector)
+
+    const mySk = Buffer.from(
+      vector.input.my_keys.private.replace('.ed25519', ''),
+      'base64'
+    )
+
+    const yourPk = Buffer.from(
+      vector.input.feed_id
+        .replace('@', '')
+        .replace('.ed25519', ''),
+      'base64'
+    )
+
+    t.deepEqual(
+      directMessageKey(mySk)(yourPk),
+      vector.output.shared_key,
+      vector.description
+    )
+  })
+
+  t.end()
+})
+
+function makeKeys () {
+  const keys = generate()
+
+  return {
+    public: Buffer.from(keys.public, 'base64'),
+    secret: Buffer.from(keys.private, 'base64')
+  }
+}
