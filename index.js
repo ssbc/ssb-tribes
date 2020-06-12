6379c58be1b73176b61dbceaaf9e07da595fee49
mixmix <whimful@gmail.com>
2020-06-12 14:06:52 +1200

    make index.js more readable

--- index.js
+++ index.js
@@ -26,165 +26,118 @@ module.exports = {
     //   groupKeys: 'sync' // should this even be public?
     // }
   },
-  init: (ssb, config) => {
-    var state = {
-      feedId: new FeedId(ssb.id).toTFK(),
-      previous: undefined,
+  init
+}
 
-      loading: {
-        previous: true,
-        keystore: true
-      },
-      get isReady () {
-        return !this.loading.keystore && !this.loading.previous
-        // we need both these things completed for:
-        // - boxing of new private messages
-        // - unboxing of new incoming messages
+function init (ssb, config) {
+  var state = {
+    feedId: new FeedId(ssb.id).toTFK(),
+    previous: undefined,
+
+    loading: {
+      previous: true,
+      keystore: true
+    },
+    get isReady () {
+      return !this.loading.keystore && !this.loading.previous
+      // we need both these things completed for:
+      // - boxing of new private messages
+      // - unboxing of new incoming messages
+    }
+  }
+
+  /* secret keys store / helper */
+  const keystore = KeyStore(join(config.path, 'private2/keystore'), ssb.keys, () => {
+    state.loading.keystore = false
+  })
+  ssb.close.hook(function (fn, args) {
+    keystore.close(() => fn.apply(this, args))
+  })
+
+  /* register the boxer / unboxer */
+  const { boxer, unboxer } = Envelope(keystore, state)
+  ssb.addBoxer(boxer)
+  ssb.addUnboxer({ init: checkReady, ...unboxer })
+  function checkReady (done) {
+    if (state.isReady) return done()
+    setTimeout(() => checkReady(done), 500)
+  }
+
+  /* start listeners */
+  listen.previous(ssb)(prev => {
+    state.previous = prev
+    if (state.loading.previous) state.loading.previous = false
+  })
+  listen.addMember(ssb)(m => {
+    const { root, groupKey } = m.value.content
+    ssb.get({ id: root, meta: true }, (err, groupInitMsg) => {
+      if (err) throw err
+
+      // WIP : should be coming in here fine...
+      const groupId = GroupId({ groupInitMsg, groupKey })
+      console.log('heard something about group:', groupId)
+    })
+  })
+
+  /* We care about group/add-member messages others have posted which:
+   * 1. add us to a new group
+   * 2. add other people to a group we're already in
+   *
+   * In (2) we may be able to skip re-indexing if they haven't published
+   * any brand new private messages since they were added.
+   * This would require knowing their feed seq at time they were entrusted with key
+   * (because they can't post messages to the group before then)
+   */
+
+  /* auto-add group tangle info to all private-group messages */
+  const getGroupTangle = GetGroupTangle(ssb, keystore)
+  ssb.publish.hook(function (fn, args) {
+    const [content, cb] = args
+    if (!content.recps) return fn.apply(this, args)
+    if (!isGroupId(content.recps[0])) return fn.apply(this, args)
+
+    getGroupTangle(content.recps[0], (err, tangle) => {
+      if (err) {
+        console.warn(err)
+        // NOTE there are two ways an err can occur in getGroupTangle, and we don't
+        // want to cb(err) with either in this hook. Rather we pass it on to boxers to throw
+        return fn.apply(this, args)
       }
-    }
 
-    /* secret keys store / helper */
-    const keystore = KeyStore(join(config.path, 'private2/keystore'), ssb.keys, () => {
-      state.loading.keystore = false
+      fn.apply(this, [set(content, 'tangles.group', tangle), cb])
     })
-    ssb.close.hook(function (fn, args) {
-      keystore.close(() => fn.apply(this, args))
-    })
+  })
 
-    /* register the boxer / unboxer */
-    const { boxer, unboxer } = Envelope(keystore, state)
-    ssb.addBoxer(boxer)
-    ssb.addUnboxer({ init: checkReady, ...unboxer })
-    function checkReady (done) {
-      if (state.isReady) return done()
-      setTimeout(() => checkReady(done), 500)
-    }
+  /* API */
+  const scuttle = Method(ssb, keystore, state) // ssb db methods
+  return {
+    group: {
+      register: keystore.group.add,
+      registerAuthors (groupId, authorIds, cb) {
+        pull(
+          pull.values(authorIds),
+          pull.asyncMap((authorId, cb) => keystore.group.addAuthor(groupId, authorId, cb)),
+          pull.collect((err) => {
+            if (err) cb(err)
+            else cb(null, true)
+          })
+        )
+      },
+      create (opts, cb) {
+        scuttle.group.init((err, data) => {
+          if (err) return cb(err)
 
-    /* start listeners */
-    listen.previous(ssb)(prev => {
-      state.previous = prev
-      if (state.loading.previous) state.loading.previous = false
-    })
-    listen.addMember(ssb)(m => {
-      const { root, groupKey } = m.value.content
-      ssb.get({ id: root, meta: true }, (err, groupInitMsg) => {
-        if (err) throw(err)
+          keystore.group.add(data.groupId, { key: data.groupKey, root: data.groupInitMsg.key }, (err) => {
+            if (err) return cb(err)
 
-        // WIP : should be coming in here fine...
-        const groupId = GroupId({ groupInitMsg, groupKey })
-
-        // NOTES FOR CHRISTIAN:
-
-        // first pass:
-        // - register the group in keystore
-        // - register the recps as being in that group
-        // - trigger rebuild
-        // - after ... indexes are up to date, do a get on an encypted message to show it's decrypted now
-
-        // second pass:
-        // - check if group is in keystore already
-        // - check if people who are being added are already known to be in group?
-        //
-        // this is where we start writing tests around scenarios we need to rebuild in
-
-        // Who do we need to add to this group?
-        //
-        // Currently we're just adding the person who created the group, but I
-        // think we probably need to add others too.
-
-        // PROBLEM: If we add the group, it complains about a duplicate.
-        //          If we don't add the group, we get:
-        //            Error: unknown group %FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF=.cloaked
-        //
-        // SOLUTION (pending): Create a `processAddMember` that creates the
-        // group if necessary and returns an array of authors that we need to
-        // rebuild.
-        if (keystore.group.get(groupId) == null) {
-          keystore.group.add(groupId, { key: groupKey, root: groupInitMsg.key }, (err) => {
-
-            if (err) throw err
-            keystore.group.addAuthor(groupId, groupInitMsg.value.author, (err) => {
-              if (err) throw err
-              ssb.rebuild(() => console.log('done rebuilding'))
+            keystore.group.addAuthor(data.groupId, ssb.id, (err) => {
+              if (err) return cb(err)
+              cb(null, data)
             })
           })
-        }
-
-
-        // calculate msgKey (groupKey)
-        // calculate readKey (msgKey)
-        // calculate groupId (feed, previous, readKey)
-        //
-        // add group to keystore (groupId, groupKey)
-        // add authors
-        //
-        // maybe make a new keystore method
-        //
-        // rebuild
-
-
-        /* We care about group/add-member messages others have posted which:
-         * 1. add us to a new group
-         * 2. add other people to a group we're already in
-         *
-         * In (2) we may be able to skip re-indexing if they haven't published
-         * any brand new private messages since they were added.
-         * This would require knowing their feed seq at time they were entrusted with key
-         * (because they can't post messages to the group before then)
-         */
-      })
-
-      /* auto-add group tangle info to all private-group messages */
-      const getGroupTangle = GetGroupTangle(ssb, keystore)
-      ssb.publish.hook(function (fn, args) {
-        const [content, cb] = args
-        if (!content.recps) return fn.apply(this, args)
-        if (!isGroupId(content.recps[0])) return fn.apply(this, args)
-
-        getGroupTangle(content.recps[0], (err, tangle) => {
-          if (err) {
-            console.warn(err)
-            // NOTE there are two ways an err can occur in getGroupTangle, and we don't
-            // want to cb(err) with either in this hook. Rather we pass it on to boxers to throw
-            return fn.apply(this, args)
-          }
-
-          fn.apply(this, [set(content, 'tangles.group', tangle), cb])
         })
-      })
-
-      /* API */
-      const scuttle = Method(ssb, keystore, state) // ssb db methods
-      return {
-        group: {
-          register: keystore.group.add,
-          registerAuthors (groupId, authorIds, cb) {
-            pull(
-              pull.values(authorIds),
-              pull.asyncMap((authorId, cb) => keystore.group.addAuthor(groupId, authorId, cb)),
-              pull.collect((err) => {
-                if (err) cb(err)
-                else cb(null, true)
-              })
-            )
-          },
-          create (opts, cb) {
-            scuttle.group.init((err, data) => {
-              if (err) return cb(err)
-
-              keystore.group.add(data.groupId, { key: data.groupKey, root: data.groupInitMsg.key }, (err) => {
-                if (err) return cb(err)
-
-                keystore.group.addAuthor(data.groupId, ssb.id, (err) => {
-                  if (err) return cb(err)
-                  cb(null, data)
-                })
-              })
-            })
-          },
-          invite: scuttle.group.addMember
-        }
-      }
-    })
+      },
+      invite: scuttle.group.addMember
+    }
   }
 }
