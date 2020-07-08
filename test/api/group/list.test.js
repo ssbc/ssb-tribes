2fa3df8788234798b7db4a9a3af7bc8d156f818b
mixmix <whimful@gmail.com>
2020-07-09 11:08:09 +1200

    move group.list test

--- test/api/group/list.test.js
+++ test/api/group/list.test.js
@@ -1,6 +1,21 @@
 const test = require('tape')
+const { Server } = require('../../helpers')
 
-test('group.list', { todo: true }, t => {
+test('list + get groups', (t) => {
+  const server = Server()
 
-  t.end()
+  server.tribes.create(null, (err, data) => {
+    t.error(err)
+    t.deepEqual(server.tribes.list(), [data.groupId])
+
+    const actualGroup = server.tribes.get(data.groupId)
+    const expectedGroup = {
+      key: data.groupKey,
+      root: data.groupInitMsg.key,
+      scheme: 'envelope-large-symmetric-group'
+    }
+    t.deepEqual(actualGroup, expectedGroup)
+
+    server.close(t.end)
+  })
 })
--- test/publish.test.js
+++ test/publish.test.js
@@ -80,20 +80,3 @@ test('publish (DM to feedId)', t => {
     })
   })
 })
-
-test('list groups', (t) => {
-  const server = Server()
-
-  server.tribes.create(null, (err, data) => {
-    t.error(err)
-    t.deepEqual(server.tribes.list(), [data.groupId])
-    const actualGroup = server.tribes.get(data.groupId)
-    const expectedGroup = {
-      key: data.groupKey,
-      root: data.groupInitMsg.key,
-      scheme: 'envelope-large-symmetric-group'
-    }
-    t.deepEqual(actualGroup, expectedGroup)
-    server.close(t.end)
-  })
-})
