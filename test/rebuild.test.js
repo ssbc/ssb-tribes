9595d37a37fefd7f07f1169c570e56d6a100e9eb
mixmix <whimful@gmail.com>
2020-08-19 13:51:34 +1200

    rebuild - check discovering more group/add-member mid rebuild works

--- test/rebuild.test.js
+++ test/rebuild.test.js
@@ -4,32 +4,47 @@ const pull = require('pull-stream')
 const { Server } = require('./helpers')
 const { FeedId } = require('../lib/cipherlinks')
 
-function replicate ({ from, to, through = noop }) {
+function replicate ({ from, to, live = true, name = abbrev, through }, done) {
   pull(
-    from.createHistoryStream({ id: from.id, live: true }),
+    from.createHistoryStream({ id: from.id, live }),
     pull.through(through),
-    pull.drain(m => {
-      to.add(m.value, (err) => {
+    pull.asyncMap((m, cb) => to.add(m.value, cb)),
+    pull.asyncMap((m, cb) => to.get({ id: m.key, private: true, meta: true }, cb)),
+    pull.drain(
+      m => {
+        const type = m.value.content.type || '?' // encrypted
+        const added = type === 'group/add-member'
+          ? `: ${m.value.content.recps.filter(r => r[0] === '@').map(name)}`
+          : ''
+        console.log(`${name(m.value.author)} -> ${name(to.id)}  |  ${abbrev(m.key)} ${type} ${added}`)
+      },
+      err => {
+        if (typeof done === 'function') return done(err)
         if (err) throw err
-        console.log(`replicated ${m.key}`)
-      })
-    })
+      }
+    )
   )
 }
-function noop () {}
-
-test('rebuild', t => {
-  const me = Server() // me
-  const admin = Server() // some friend
-
-  var messages = []
-  replicate({
-    from: admin,
-    to: me,
-    through: m => messages.push(m.key)
+function abbrev (key) { return key.slice(0, 9) }
+function nMessages (n, { type = 'post', recps } = {}) {
+  return new Array(20).fill(type).map((val, i) => {
+    const content = { type, count: i }
+    if (recps) content.recps = recps
+    return content
   })
+}
 
-  var groupId
+test('rebuild (I am added to a group)', t => {
+  const admin = Server()
+  const me = Server()
+  const name = (id) => {
+    switch (id) {
+      case admin.id: return 'admin'
+      case me.id: return 'me'
+    }
+  }
+
+  replicate({ from: admin, to: me, name })
 
   me.rebuild.hook(function (rebuild, [cb]) {
     t.pass('I automatically call a rebuild')
@@ -42,7 +57,7 @@ test('rebuild', t => {
         me.createUserStream({ id: admin.id, private: true }),
         pull.drain(
           m => {
-            t.equal(typeof m.value.content, 'object', `I auto-unbox msg of type: ${m.value.content.type}`)
+            t.equal(typeof m.value.content, 'object', `I auto-unbox msg: ${m.value.content.type}`)
           },
           (err) => {
             if (err) throw err
@@ -59,11 +74,111 @@ test('rebuild', t => {
   admin.tribes.create({}, (err, data) => {
     if (err) throw err
 
-    groupId = data.groupId
-    console.log(`created group: ${groupId}`)
-
-    admin.tribes.invite(groupId, [me.id], { text: 'ahoy' }, (err, invite) => {
+    admin.tribes.invite(data.groupId, [me.id], { text: 'ahoy' }, (err, invite) => {
       t.error(err, 'admin adds me to group')
+      if (err) throw err
+    })
+  })
+})
+
+test('rebuild (I am added to a group)', t => {
+  const admin = Server()
+  const alice = Server()
+  const bob = Server()
+  const name = (id) => {
+    switch (id) {
+      case admin.id: return 'admin'
+      case alice.id: return 'alice'
+      case bob.id: return 'bob  '
+    }
+  }
+
+  var groupId
+  replicate({ from: admin, to: alice, name })
+  replicate({ from: admin, to: bob, name })
+
+  alice.rebuild.hook(function (rebuild, [cb]) {
+    rebuild(() => {
+      t.pass('alice is in the group')
+
+      cb()
+      pull(
+        pull.values(nMessages(20, { type: 'alice', recps: [groupId] })),
+        pull.asyncMap(alice.publish),
+        pull.collect((err) => {
+          t.error(err, 'alice publishes to the group')
+
+          replicate({ from: alice, to: bob, name, live: false }, (err) => {
+            t.error(err, 'bob has received all of admin + alices messages to date')
+            alice.close()
+            // NOTE: we close alice here to stop her from re-indexing
+            // when we add bob is added to the group
+            // If you close while rebuilding, you get a segmentation fault
+            admin.tribes.invite(groupId, [bob.id], { text: 'hi!' }, (err) => {
+              t.error(err, 'admin adds bob to the group')
+            })
+          })
+        })
+      )
+    })
+  })
+
+  var rebuildCount = 0
+  bob.rebuild.hook(function (rebuild, [cb]) {
+    const _count = ++rebuildCount
+
+    switch (_count) {
+      case 1:
+        t.pass('bob calls rebuild (added to group)')
+        break
+      case 2:
+        t.pass('bob calls rebuild (realises alice is in group)')
+        break
+      default:
+        t.fail(`rebuild called to many times: ${_count}`)
+    }
+
+    rebuild(() => {
+      cb()
+      if (_count !== 2) return
+
+      let seenAlices = 0
+      pull(
+        bob.createLogStream({ private: true }),
+        pull.map(m => m.value.content),
+        pull.drain(
+          ({ type, count, recps }) => {
+            let comment = `bob auto-unboxes: ${type} `
+
+            if (type === 'group/add-member') {
+              comment += `: ${recps.filter(r => r[0] === '@').map(name)}`
+            }
+            if (type === 'alice') {
+              seenAlices++
+              if (count === 0 || count === 19) comment += `(${count})`
+              else if (count === 1) comment += '...'
+              else return
+            }
+            t.true(type, comment)
+          },
+          (err) => {
+            if (seenAlices === 20) t.equal(seenAlices, 20, 'saw 20 messages from alice')
+            if (err) throw err
+            bob.close()
+            admin.close()
+            t.end()
+          }
+        )
+      )
+    })
+  })
+
+  admin.tribes.create({}, (err, data) => {
+    if (err) throw err
+
+    groupId = data.groupId
+    admin.tribes.invite(groupId, [alice.id], { text: 'ahoy' }, (err) => {
+      t.error(err, 'admin adds alice to group')
       if (err) throw err
     })
   })
