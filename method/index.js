ff9c5fbb08782cf120d18f87ee3a56876b0d6b61
chereseeriepa <cherese.eriepa@outlook.com>
2021-08-11 10:21:28 +1200

    refactor links to use ssb-crut

--- method/index.js
+++ method/index.js
@@ -1,11 +1,15 @@
+const Crut = require('ssb-crut')
 const Init = require('./group/init')
 const AddMember = require('./group/add-member')
-const { CreateFeedGroupLink, CreateGroupSubgroupLink } = require('./link/create')
 const FindByGroupByFeedId = require('./link/find-group-by-feedid')
 const Application = require('./application')
+const FeedGroupLink = require('../spec/link/feed-group')
+const GroupSubgroupLink = require('../spec/link/group-subgroup')
 
 module.exports = function Method (ssb, keystore, state) {
   const application = Application(ssb)
+  const linkFeedCrut = new Crut(ssb, FeedGroupLink)
+  const linkSubgroupCrut = new Crut(ssb, GroupSubgroupLink)
 
   return {
     group: {
@@ -13,8 +17,36 @@ module.exports = function Method (ssb, keystore, state) {
       addMember: patient(AddMember(ssb, keystore, state))
     },
     link: {
-      create: patient(CreateFeedGroupLink(ssb)),
-      createSubgroupLink: patient(CreateGroupSubgroupLink(ssb)),
+      create ({ group, name }, cb) {
+        const input = {
+          parent: ssb.id,
+          child: group,
+          name,
+          recps: [group]
+        }
+
+        // set the recps on the link to be the same as the recps on the settings
+        if (!name) delete input.name
+
+        patient(linkFeedCrut.create.bind(linkFeedCrut)(input, (err, linkId) => {
+          if (err) return cb(err)
+
+          linkFeedCrut.read(linkId, cb)
+        }))
+      },
+      createSubgroupLink: ({ group, subgroup }, cb) => {
+        const input = {
+          parent: group,
+          child: subgroup,
+          recps: [group]
+        }
+
+        patient(linkSubgroupCrut.create.bind(linkSubgroupCrut)(input, (err, linkId) => {
+          if (err) return cb(err)
+
+          linkSubgroupCrut.read(linkId, cb)
+        }))
+      },
       findGroupByFeedId: FindByGroupByFeedId(ssb)
     },
     // TODO - rm patient from these?
--- spec/link/feed-group/index.js
+++
@@ -1,6 +0,0 @@
-const Validator = require('is-my-ssb-valid')
-const schema = require('./schema')
-
-module.exports = {
-  isRoot: Validator(schema.root)
-}
