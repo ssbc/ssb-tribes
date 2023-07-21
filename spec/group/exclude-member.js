d23651443c8c1d821ee15445b7ad1c935b428e5d
Jacob Karlsson <jacob.karlsson95@gmail.com>
2023-07-21 13:38:37 +0200

    Also use sigil msg links in exclude msgs

--- spec/group/exclude-member.js
+++ spec/group/exclude-member.js
@@ -2,10 +2,10 @@ const Validator = require('is-my-ssb-valid')
 const addMemberSchema = require('private-group-spec/group/add-member/v1/schema.json')
 const schema = require('private-group-spec/group/exclude-member/schema.json')
 
+// we use the tribes2 exclude message type but with sigil links instead of URIs
 schema.definitions.feedId = addMemberSchema.definitions.feedId
 schema.definitions.groupId = addMemberSchema.definitions.cloakedMessageId
-
-console.log('exclude member schema', schema)
+schema.definitions.messageId = addMemberSchema.definitions.messageId
 
 module.exports = {
   isValid: Validator(schema)
