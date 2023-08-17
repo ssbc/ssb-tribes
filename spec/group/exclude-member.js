64d7d5b239f06eb62df8291cef8cf5bfcc0a310e
Jacob Karlsson <jacob.karlsson95@gmail.com>
2023-08-17 15:22:42 +0200

    Don't directly mutate exclusion schema
    Co-authored-by: mix irving <whimful@gmail.com>

--- spec/group/exclude-member.js
+++ spec/group/exclude-member.js
@@ -1,11 +1,17 @@
 const Validator = require('is-my-ssb-valid')
 const addMemberSchema = require('private-group-spec/group/add-member/v1/schema.json')
-const schema = require('private-group-spec/group/exclude-member/schema.json')
+const baseSchema = require('private-group-spec/group/exclude-member/schema.json')
 
 // we use the tribes2 exclude message type but with sigil links instead of URIs
-schema.definitions.feedId = addMemberSchema.definitions.feedId
-schema.definitions.groupId = addMemberSchema.definitions.cloakedMessageId
-schema.definitions.messageId = addMemberSchema.definitions.messageId
+const schema = {
+  ...baseSchema,
+  definitions: {
+    ...baseSchema.definitions
+    feedId: addMemberSchema.definitions.feedId
+    groupId: addMemberSchema.definitions.cloakedMessageId
+    messageId: addMemberSchema.definitions.messageId
+  }
+}
 
 module.exports = {
   isValid: Validator(schema)
