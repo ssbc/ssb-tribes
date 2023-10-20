68323e9fb3e3da2f659f4b9e076160272996ff13
Jacob Karlsson <jacob.karlsson95@gmail.com>
2023-10-20 11:11:56 +0200

    Remove envelope file

--- test/unbox.test.js
+++ test/unbox.test.js
@@ -5,8 +5,6 @@ const pull = require('pull-stream')
 const { promisify: p } = require('util')
 const { decodeLeaves, Server, Run } = require('./helpers')
 
-const envelope = require('../envelope')
-
 const vectors = [
   require('./vectors/unbox1.json'),
   require('./vectors/unbox2.json')
@@ -70,48 +68,5 @@ test.skip('unbox', async t => {
   t.deepEqual(backlinks, RECPS, 'backlinks indexed all messages (unboxed!)')
 
   await run('close ssb', p(ssb.close)(true))
-  t.end()
-})
-
-// TODO: this tests envelope() which we're probably gonna remove in favor of box2, so we should remove this test too right?
-test.skip('unbox - test vectors', async t => {
-  console.log('vectors:', vectors.length)
-
-  vectors.forEach(vector => {
-    const { msgs, trial_keys } = vector.input
-
-    const mockKeyStore = {
-      group: {
-        listSync: () => ['a'],
-        get: () => ({
-          readKeys: trial_keys
-        })
-      },
-      dm: {
-        has: () => true,
-        get: () =>
-          ({ key: Buffer.alloc(32), scheme: 'junk' }) // just here to stop code choking
-      }
-    }
-
-    const mockState = {
-      keys: {
-        curve: 'ed25519',
-        public: '3Wr/Swdt8vTC4NdOWJKaIX2hU2qcarSSdFpV4eyJKVw=.ed25519',
-        private: 'rlg3ciKZRCcYLjbDfy4eymsvNzCHRXDfWw65PvttqiXdav9LB23y9MLg105YkpohfaFTapxqtJJ0WlXh7IkpXA==.ed25519',
-        id: '@3Wr/Swdt8vTC4NdOWJKaIX2hU2qcarSSdFpV4eyJKVw=.ed25519'
-      }
-    }
-
-    const { unboxer } = envelope(mockKeyStore, mockState)
-    const ciphertext = msgs[0].value.content
-
-    const read_key = unboxer.key(ciphertext, msgs[0].value)
-    const body = unboxer.value(ciphertext, msgs[0].value, read_key)
-    t.deepEqual(body, vector.output.msgsContent[0], vector.description)
-  })
-
-  console.log('DONE')
-
   t.end()
 })
